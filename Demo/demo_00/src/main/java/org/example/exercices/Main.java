68ea0613cb47f96fd73cb8fec0f4db2b11168bc5
loic <loic.gstrn@gmail.com>
2023-11-21 09:07:17 +0100

    exo string

--- Demo/demo_00/src/main/java/org/example/exercices/Main.java
+++ Demo/demo_00/src/main/java/org/example/exercices/Main.java
@@ -2,8 +2,8 @@ package org.example.exercices;
 
 public class Main {
     public static void main(String[] args){
-        ExerciceString1.ComptageDeMot("Bonjour Toto");
-        ExerciceString1.ComptageOccurence("maisonnette", 't');
+        ExerciceString1.comptageDeMot("Bonjour Toto");
+        ExerciceString1.comptageOccurence("maisonnette", 't');
         //ExerciceString1.Anagramme("guerison", "soigneur");
         //ExerciceString1.Palindrome("kayak");
         //ExerciceString1.Pyramide2(6);
